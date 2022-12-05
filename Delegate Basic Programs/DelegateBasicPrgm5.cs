dbdd6ed8c1a6dfd1ab5e224f9452eed5f1663315
Megha-Su <119593325+Megha-Su@users.noreply.github.com>
2022-12-05 12:55:56 +0530

    Add files via upload

---
+++ Delegate Basic Programs/DelegateBasicPrgm5.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DelegateBasicPrgm5
+{
+
+    public delegate int SumDelegate(int firstnumber,int secondnumber);
+
+   
+    class Program
+    {
+        public static int Sum(int firstnumber, int secondnumber)
+        {
+            int sum = firstnumber + secondnumber;
+            return sum;
+
+        }
+        static void Main(string[] args)
+        {
+            var sumdelegate = new SumDelegate(Sum);
+            var sum = sumdelegate(35, 7);
+            Console.WriteLine(sum);
+
+        }
+    }
+}

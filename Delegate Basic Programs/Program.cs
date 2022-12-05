c6897e3078683de1618af5d152da5427d6b7ad97
megha.su <megha.su@PIA3298>
2022-12-05 11:19:27 +0530

    Add project files.

---
+++ Delegate Basic Programs/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Delegate_Basic_Programs
+{
+    public delegate void Namedelegate();
+    class Program
+    {
+        public static  void Name()
+        {
+            Console.WriteLine("Megha");
+        }
+
+        static void Main(string[] args)
+        {
+            var name = new Namedelegate(Name);
+            name();
+        }
+    }
+}
